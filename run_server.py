d104ad9cc41b0249af2cacdbf99ed88f355c4971
Apostolos Psaros <afpsaros@gmail.com>
2024-09-03 15:43:01 -0400

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,26 +1,41 @@
 import webbrowser
 import subprocess
 import sys
+import time
+import requests
 
 def run_server():
-    # Start the Flask server using subprocess
     server_process = subprocess.Popen([sys.executable, 'server.py'])
     return server_process
 
 def open_browser():
-    # Automatically open the default web browser to the Flask app
     webbrowser.open_new('http://127.0.0.1:5000')
 
-def main():
-    server_process = run_server()
-    open_browser()
+def wait_for_server():
+    url = 'http://127.0.0.1:5000'
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                break
+        except requests.ConnectionError:
+            time.sleep(1)
 
+def main():
     try:
-        # Keep the script running while the server is active
+        server_process = run_server()
+        wait_for_server()
+        open_browser()
+
         server_process.communicate()
+
     except KeyboardInterrupt:
         server_process.terminate()
         print("\nServer stopped.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        server_process.terminate()
 
 if __name__ == '__main__':
     main()
